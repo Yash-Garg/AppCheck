a90ce19aad51e4f4e2ff5f9c041b19018a42daea
Yash-Garg <yash.garg@outlook.in>
2022-03-17 20:58:46 +0530

    chore: get context from pluginBinding & upgrade AGP

--- android/src/main/kotlin/dev/yashgarg/appcheck/AppcheckPlugin.kt
+++ android/src/main/kotlin/dev/yashgarg/appcheck/AppcheckPlugin.kt
@@ -1,18 +1,10 @@
 package dev.yashgarg.appcheck
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.PackageInfo;
-import android.content.pm.ApplicationInfo;
-import android.os.Build;
-import android.annotation.TargetApi;
+import android.content.Context
+import kotlin.collections.*
+import android.content.pm.PackageManager
+import android.content.pm.PackageInfo
+import android.content.pm.ApplicationInfo
 import androidx.annotation.NonNull
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
@@ -28,44 +20,36 @@ class AppcheckPlugin : FlutterPlugin, MethodCallHandler {
     /// This local reference serves to register the plugin with the Flutter Engine and unregister it
     /// when the Flutter Engine is detached from the Activity
     private lateinit var channel: MethodChannel
+    private lateinit var context: Context
 
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         channel = MethodChannel(flutterPluginBinding.binaryMessenger, "dev.yashgarg/appcheck")
         channel.setMethodCallHandler(this)
+        context = flutterPluginBinding.applicationContext
     }
 
     override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-        val uriSchema: String
         when (call.method) {
-            "checkAvailability" -> {
-                uriSchema = call.argument("uri").toString()
-                checkAvailability(uriSchema, result)
-            }
+//            "checkAvailability" -> {
+//                uriSchema = call.argument("uri").toString()
+//                checkAvailability(uriSchema, result)
+//            }
             "getInstalledApps" -> result.success(installedApps)
-            "isAppEnabled" -> {
-                uriSchema = call.argument("uri").toString()
-                isAppEnabled(uriSchema, result)
-            }
-            "launchApp" -> {
-                uriSchema = call.argument("uri").toString()
-                launchApp(uriSchema, result)
-            }
+//            "isAppEnabled" -> {
+//                uriSchema = call.argument("uri").toString()
+//                isAppEnabled(uriSchema, result)
+//            }
+//            "launchApp" -> {
+//                uriSchema = call.argument("uri").toString()
+//                launchApp(uriSchema, result)
+//            }
             else -> result.notImplemented()
         }
     }
 
-    private fun checkAvailability(uri: String, result: Result) {
-        val info = getAppPackageInfo(uri)
-        if (info != null) {
-            result.success(convertPackageInfoToJson(info))
-            return
-        }
-        result.error("", "App not found $uri", null)
-    }
-
-    private val installedApps: List<Map<String, Any>>
-        private get() {
-            val packageManager: PackageManager = registrar.context().getPackageManager()
+    private val installedApps: MutableList<Map<String, Any>>
+        get() {
+            val packageManager: PackageManager = context.packageManager
             val apps = packageManager.getInstalledPackages(0)
             val installedApps: MutableList<Map<String, Any>> = ArrayList(apps.size)
             val systemAppMask =
@@ -80,55 +64,14 @@ class AppcheckPlugin : FlutterPlugin, MethodCallHandler {
             return installedApps
         }
 
-    private fun getAppPackageInfo(uri: String): PackageInfo? {
-        val ctx = activity.applicationContext
-        val pm = ctx.packageManager
-        try {
-            return pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES)
-        } catch (e: PackageManager.NameNotFoundException) {
-        }
-        return null
-    }
-
     private fun convertPackageInfoToJson(info: PackageInfo): Map<String, Any> {
         val map: MutableMap<String, Any> = HashMap()
         map["app_name"] =
-            info.applicationInfo.loadLabel(registrar.context().getPackageManager()).toString()
+            info.applicationInfo.loadLabel(context.packageManager).toString()
         map["package_name"] = info.packageName
         map["version_code"] = info.versionCode.toString()
         map["version_name"] = info.versionName
         return map
-    }
-
-    private fun isAppEnabled(packageName: String, result: Result) {
-        var appStatus = false
-        try {
-            val ai: ApplicationInfo =
-                registrar.context().getPackageManager().getApplicationInfo(packageName, 0)
-            if (ai != null) {
-                appStatus = ai.enabled
-            }
-        } catch (e: PackageManager.NameNotFoundException) {
-            result.error("", e.message + " " + packageName, e)
-            return
-        }
-        result.success(appStatus)
-    }
-
-    @TargetApi(Build.VERSION_CODES.CUPCAKE)
-    private fun launchApp(packageName: String, result: Result) {
-        val info = getAppPackageInfo(packageName)
-        if (info != null) {
-            val launchIntent: Intent =
-                registrar.context().getPackageManager().getLaunchIntentForPackage(packageName)
-            if (launchIntent != null) {
-                launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                registrar.context().startActivity(launchIntent)
-                result.success(null)
-                return
-            }
-        }
-        result.error("", "App not found $packageName", null)
     }
 
     override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
